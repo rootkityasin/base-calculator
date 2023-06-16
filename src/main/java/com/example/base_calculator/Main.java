1e9ddc0e864aa071c4321cbaff94d44ae072e688
rootkityasin <yasinarafatnew@gmail.com>
2023-06-17 02:20:44 +0600

    doing setup

--- src/main/java/com/example/base_calculator/Main.java
+++ src/main/java/com/example/base_calculator/Main.java
@@ -1,18 +1,19 @@
 package com.example.base_calculator;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Main.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Base Conversion Calculator");
         stage.setScene(scene);
         stage.show();
     }
@@ -20,4 +21,6 @@ public class HelloApplication extends Application {
     public static void main(String[] args) {
         launch();
     }
+
+
 }
---
+++ src/main/java/com/example/base_calculator/Main_controller.java
@@ -0,0 +1,17 @@
+package com.example.base_calculator;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class Main_controller {
+    @FXML
+
+    private Button Exit;
+
+    @FXML
+    void do_exit(ActionEvent event) {
+        System.exit(0);
+    }
+}
